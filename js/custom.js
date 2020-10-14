eb1d1eb51445509361a20bc4ecd4824d25221fc7
Hossam Hilal <46751584+hossamhilal@users.noreply.github.com>
2020-10-14 23:42:16 +0300

    update

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,27 @@
 $(document).ready(function ($) {
     'use strict';
 
+     // OPEN SIDE  MENU 
+     $('.menu-btn').on('click', function(){
+        let logo = $('.logo');
+        logo.clone().insertBefore($('.nav-menu ul'));
+        $('.nav-menu ').toggleClass('show');
+        $('.nav-overlay').addClass('show');  
+        setTimeout(function(){
+            $('body').addClass('stopScroll');
+        }, 200); 
+    });
+
+    // CLOSE SIDE MENU 
+    $('.nav-overlay').on('click', function(){
+        let logo = $('.nav-menu .logo');
+        $('.nav-menu').find(logo).remove();
+        $(this).removeClass('show');
+        $('.nav-menu ').removeClass('show');  
+        $('body').removeClass('stopScroll');  
+    });
+
+    
     // iniat WOW Js
     new WOW().init();
 
